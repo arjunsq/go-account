556efa489d042732c2ad7777a6b0f71c27e63939
ARJUN K S <arjun@ks.ks>
2018-08-09 18:24:17 +0530

    Another

--- functions.go
+++ functions.go
@@ -3,39 +3,28 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"net/http"
-	"os"
-	"strings"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	bs, err := ioutil.ReadFile("index.html")
-	if err != nil {
-		os.Exit(1)
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("signup.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// logic part of log in
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+		responsevalue := "Username: " + r.Form["username"][0] + " Password: " + r.Form["password"][0] + " registered successfully"
+		fmt.Fprintf(w, responsevalue) // write data to response
 	}
-	fmt.Fprintf(w, string(bs))
-	fmt.Printf("Reloaded Hello World\n")
 }
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
-	// attention: If you do not call ParseForm method, the following data can not be obtained form
-	fmt.Println(r.Form) // print information on server side.
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") // write data to response
-}
-
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.html")
+		fmt.Println("Inside login")
+		t, _ := template.ParseFiles("index.html")
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", helloWorld)
-	http.HandleFunc("/sayhello", sayhelloName) // setting router rule
-	http.HandleFunc("/login", login)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/", login)
 	http.ListenAndServe(":9000", nil)
 }
